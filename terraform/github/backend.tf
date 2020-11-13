fd19169bc215abaf0f5ba6abea39c3fe34588530
Sora Morimoto <sora@morimoto.io>
2020-11-13 17:45:55 +0900

    Update the backend bucket setup
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- terraform/github/backend.tf
+++ terraform/github/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket                      = "asdf-infrastructure"
+    bucket                      = "asdf-community-infrastructure"
     endpoint                    = "https://s3.fr-par.scw.cloud"
     iam_endpoint                = "https://s3.fr-par.scw.cloud"
-    key                         = "github"
+    key                         = "github.tfstate"
     region                      = "fr-par"
     skip_credentials_validation = true
     skip_region_validation      = true
