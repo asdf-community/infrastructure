904c3b85e9a7ae8d9efc11c4670819fb023af66c
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2024-01-09 23:42:04 +0800

    feat(repository): add asdf-regal repo management

--- terraform/github/repositories.tf
+++ terraform/github/repositories.tf
@@ -928,6 +928,20 @@ locals {
       ]
     }
 
+    asdf-regal = {
+      description    = "regal plugin for the asdf version manager"
+      homepage_url   = "https://github.com/asdf-vm/asdf"
+      default_branch = "main"
+      topics = [
+        "asdf-plugin",
+        "asdf",
+      ]
+      teams = [
+        "asdf-regal",
+        "asdf-core",
+      ]
+    }
+
     asdf-rlwrap = {
       description    = "rlwrap plugin for the asdf version manager"
       homepage_url   = "https://github.com/asdf-vm/asdf"
