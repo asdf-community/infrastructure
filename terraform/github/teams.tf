4f9654346e316c485464c250cfed716f5fd5951e
Stephan Meijer <me@stephanmeijer.com>
2024-05-28 19:03:59 +0200

    Change asdf-pandoc to be maintained by StephanMeijer
    asdf-pandoc does not seem to have been claimed as community repository. I would like to do so so that I can work on my changes as a community effort.

--- terraform/github/teams.tf
+++ terraform/github/teams.tf
@@ -341,7 +341,7 @@ locals {
     asdf-pandoc = {
       description = "The people with push access to the asdf-pandoc repository",
       maintainers = [
-        "sys9kdr",
+        "StephanMeijer",
       ]
     }
 
