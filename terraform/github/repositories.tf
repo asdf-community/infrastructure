c30d5052d173f1a777e5dae2b9b47a9038b40dcd
Albert <87888006+MustCodeAl@users.noreply.github.com>
2025-12-23 15:05:51 -0600

    Add asdf-manpager plugin configuration

--- terraform/github/repositories.tf
+++ terraform/github/repositories.tf
@@ -689,6 +689,21 @@ locals {
         ]
       }
 
+       asdf-manpager = {
+        description    = "Manpager plugin for the asdf version manager"
+        homepage_url   = "https://github.com/asdf-vm/asdf"
+        default_branch = "master"
+        topics = [
+          "asdf-plugin",
+          "asdf",
+        ]
+        teams = [
+          "asdf-manpager",
+          "asdf-core",
+        ]
+      }
+
+
       asdf-meson = {
         description    = "Meson plugin for the asdf version manager"
         homepage_url   = "https://github.com/asdf-vm/asdf"
